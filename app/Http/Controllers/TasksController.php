2a73b6f24b70b254683e37490beb8a9a5f289d1a
hirahei <tech-study@outlook.jp>
2018-06-05 13:20:53 +0000

    bootstrap

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -45,7 +45,8 @@ class TasksController extends Controller
     public function store(Request $request)
     {   
         $this->validate($request,[
-            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+            'content' => 'required|max:10',
         ]);
         
         $task = new Task;
@@ -96,10 +97,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {   
         $this->validate($request,[
-            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+            'content' => 'required|max:10',
         ]);
         
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,15 +2,24 @@
 
 @section('content')
 
-    <h1>タスク一覧</h1>
-    
-    @if (count($tasks) > 0)
-        <ul>
-            @foreach ($tasks as $task)
-                <li>{!! link_to_route('tasks.show',$task->id,['id' => $task->id]) !!}:{{ $task->content }}</li>
-                
-            @endforeach
-        </ul>
-    @endif
-    {!! link_to_route('tasks.create','新規タスクの追加') !!}
+   @if (count($tasks) > 0)
+       <table class="table table-striped">
+           <thead>
+               <tr>
+                   <th>id</th>
+                   <th>タイトル</th>
+               </tr>
+           </thead>
+           <tbody>
+               @foreach ($tasks as $task)
+                   <tr>
+                       <td>{!! link_to_route('tasks.show',$task->id,['id' => $task->id]) !!}</td>
+                       <td>{{ $task->status }}</td>
+                       <td>{{ $task->content }}</td>
+                   </tr>
+               @endforeach
+           </tbody>
+       </table>
+   @endif
+   {!! link_to_route('tasks.create','新規タスクの追加',null,['class' => 'btn btn-primary']) !!}
 @endsection
